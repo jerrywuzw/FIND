0eb93cbc1abc188b2b2ace67876055d0d5e17e1b
zwu3 <99296226+zwu3@users.noreply.github.com>
2023-10-27 23:26:10 -0700

    Initial Commit

--- FIND/ViewController.swift
+++ FIND/ViewController.swift
@@ -1,19 +1,93 @@
-//
-//  ViewController.swift
-//  FIND
-//
-//  Created by Jerry Wu on 10/22/23.
-//
-
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let locationManager = CLLocationManager()
+    var previousLocation: CLLocation?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        mapView.delegate = self
+        checkLocationServices()
     }
 
+    func setupLocationManager() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    }
+    
+    func checkLocationServices() {
+        if CLLocationManager.locationServicesEnabled() {
+            setupLocationManager()
+            checkLocationAuthorization()
+        } else {
+            showAlertWith(title: "Location Services Disabled", message: "Please enable Location Services in Settings")
+        }
+    }
+    
+    func checkLocationAuthorization() {
+        switch locationManager.authorizationStatus {
+        case .authorizedWhenInUse, .authorizedAlways:
+            mapView.showsUserLocation = true
+            locationManager.startUpdatingLocation()
+            previousLocation = getCenterLocation(for: mapView)
+        case .denied:
+            showAlertWith(title: "Location Access Denied", message: "Please enable access to your location in Settings")
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        case .restricted:
+            showAlertWith(title: "Location Access Restricted", message: "Location access has been restricted. You may need to check parental controls or device policies.")
+        @unknown default:
+            break
+        }
+    }
+    
+    func getCenterLocation(for mapView: MKMapView) -> CLLocation {
+        return CLLocation(latitude: mapView.centerCoordinate.latitude, longitude: mapView.centerCoordinate.longitude)
+    }
 
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
+        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+        
+        DispatchQueue.main.async { [weak self] in
+            self?.mapView.setRegion(region, animated: true)
+        }
+        
+        if let previousLocation = self.previousLocation {
+            if location.distance(from: previousLocation) > 10 {
+                let polyline = MKPolyline(coordinates: [previousLocation.coordinate, location.coordinate], count: 2)
+                mapView.addOverlay(polyline)
+                self.previousLocation = location
+            }
+        } else {
+            previousLocation = location
+        }
+    }
+
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        checkLocationAuthorization()
+    }
+    
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        if overlay is MKPolyline {
+            let renderer = MKPolylineRenderer(overlay: overlay)
+            renderer.strokeColor = .blue
+            renderer.lineWidth = 4
+            return renderer
+        }
+        return MKOverlayRenderer()
+    }
+
+    func showAlertWith(title: String, message: String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(okAction)
+        present(alertController, animated: true, completion: nil)
+    }
 }
-
